49968e5fe05b573734940918883913f777dc10c7
Greg Alton <gregaltonemail@gmail.com>
2024-02-18 07:15:20 -0600

    Added comments to explain how extension and protocol conformance implement the abstraction.

---
+++ GABLEManager/GABLEManager/GABLEPeripheralProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  GABLEPeripheralProtocol.swift
+//  GABLEManager
+//
+//  Created by Greg Alton on 2/17/24.
+//
+
+import Foundation
---
+++ GABLEManager/GABLEManagerTests/GABLEMockPeripheral.swift
@@ -0,0 +1,8 @@
+//
+//  GABLEMockPeripheral.swift
+//  GABLEManagerTests
+//
+//  Created by Greg Alton on 2/17/24.
+//
+
+import Foundation
